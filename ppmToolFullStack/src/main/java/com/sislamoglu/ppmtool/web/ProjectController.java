504d891d5b8955b41f47b6a357d299815f0afa47
unknown <Serkan Islamoglu>
2018-10-27 12:58:45 +0300

    added bindingResult and object validation

--- ppmToolFullStack/src/main/java/com/sislamoglu/ppmtool/web/ProjectController.java
+++ ppmToolFullStack/src/main/java/com/sislamoglu/ppmtool/web/ProjectController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/api/project")
@@ -18,8 +21,13 @@ public class ProjectController {
     ProjectService projectService;
 
     @RequestMapping(method = RequestMethod.POST, value = "/")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
-        Project aProject = projectService.saveOrUpdateProject(project);
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+        }
+        Project newProject = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
+
+
 }
