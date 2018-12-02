6c5e58ea1efc002273b239d29f2abb0efb44393b
Serkan Islamoglu <serkanislamoglu94@gmail.com>
2018-12-02 23:07:42 +0300

    rehresh issue is solved

---
+++ ppmToolFullStack/src/main/resources/static/precache-manifest.2e3be3d0a6daa1aecb4b638ef0a46476.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8b86447adbb2ea655e7a",
+    "url": "/static/js/main.8b86447a.chunk.js"
+  },
+  {
+    "revision": "e2fe23aa59ae016a54bd",
+    "url": "/static/js/1.e2fe23aa.chunk.js"
+  },
+  {
+    "revision": "8b86447adbb2ea655e7a",
+    "url": "/static/css/main.14739637.chunk.css"
+  },
+  {
+    "revision": "e2fe23aa59ae016a54bd",
+    "url": "/static/css/1.b344b4ed.chunk.css"
+  },
+  {
+    "revision": "7c4d99006f74011ab8b288faa7908f0b",
+    "url": "/index.html"
+  }
+];
